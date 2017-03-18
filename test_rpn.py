4d0b3fcf8954edef524bde5de8f8f7f11095689c
Ben <bteisman@umich.edu>
2017-03-18 16:51:33 -0400

    fix

--- test_rpn.py
+++ test_rpn.py
@@ -12,9 +12,6 @@ class TestBasics(unittest.TestCase):
 	def test_exponent(self):
 		result = rpn.calculate('2 3 ^')
 		self.assertEqual(8, result)
-	def test_multiply(self):
-		result = rpn.calculate('2 3 *')
-		self.assertEqual(6, result)
 
 if __name__ == '__main__':
 	unittest.main()
